37fbb0611abef8f1c94b28253c3a10c26d5210b1
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2023-11-29 16:25:30 +0000

    comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ]
   return (
     <div className="App">
-     
+     <Todos items={todos} />
     </div>
   );
 }
